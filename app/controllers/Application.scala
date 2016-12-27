7a6adac1b5c88842854e36e22b23d97a292902e3
enghwa <enghwa@aws>
2016-12-27 17:06:21 +0800

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new scala application is ready."))
+  }
+
+  def getName = Action {
+    Ok("John")
+  }
+
+}
